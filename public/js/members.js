fcc3fbfd3acda19438aa479b9f0bb158a78814e3
Sam Ayler <sayler8701@gmail.com>
2021-01-23 19:12:02 -0800

    adding functionality to add a new user

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,35 @@ $(document).ready(function () {
 	$.get("/api/user_data").then(function (data) {
 		$(".member-name").text(data.housename);
 	});
+
+	// Getting references to our form and input
+	const addUser = $("form.add");
+	const nameInput = $("input#name-input");
+
+	// Event listener for button to add new user
+	addUser.on("submit", (e) => {
+		e.preventDefault();
+		const userData = {
+			name: nameInput.val().trim(),
+		};
+
+		inputUser(userData.name);
+		nameInput.val("");
+	});
+
+	// Function to add new user
+	function inputUser(name) {
+		$.post("/api/housemember", {
+			name: name,
+		})
+			.then((data) => {
+				console.log("A new user has been added!");
+			})
+			.catch(handleLoginErr);
+	}
+
+	function handleLoginErr(err) {
+		$("#alert .msg").text(err.responseJSON);
+		$("#alert").fadeIn(500);
+	}
 });
