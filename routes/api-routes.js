e041cf91e10eae0943aa39e017c90c592933fa4f
Sam Ayler <sayler8701@gmail.com>
2021-01-23 17:27:23 -0800

    updating routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -48,19 +48,20 @@ module.exports = function (app) {
 	});
 
 	// Route to get all users for house hold
-	app.get("api/housemember", (req, res) => {
+	app.get("/api/housemember", (req, res) => {
 		db.HouseMember.findAll({}).then((dbHouseMember) => res.json(dbHouseMember));
+		console.log("stuff happened");
 	});
 
 	// Route to create Housememeber
-	app.post("api/housememeber", (req, res) => {
+	app.post("/api/housemember", (req, res) => {
 		db.HouseMember.create({
 			name: req.body.name,
 		}).then((dbHouseMember) => res.json(dbHouseMember));
 	});
 
 	// Route to delete a housememeber
-	app.delete("api/housemember/:id", (req, res) => {
+	app.delete("/api/housemember/:id", (req, res) => {
 		db.HouseMember.destroy({
 			where: {
 				id: req.params.id,
@@ -69,22 +70,15 @@ module.exports = function (app) {
 	});
 
 	// Route to create a Task
-	app.post("api/task", (req, res) => {
+	app.post("/api/task", (req, res) => {
 		db.Task.create({
 			title: req.body.title,
 			description: req.body.description,
-			monday: false,
-			tuesday: false,
-			wednesday: false,
-			thursday: false,
-			friday: false,
-			saturday: false,
-			sunday: false,
 		}).then((dbTask) => res.json(dbTask));
 	});
 
 	// Route to delete a Task
-	app.delete("api/task/:id", (req, res) => {
+	app.delete("/api/task/:id", (req, res) => {
 		db.Task.destroy({
 			where: {
 				id: req.params.id,
@@ -93,7 +87,7 @@ module.exports = function (app) {
 	});
 
 	// Route to update a Task
-	app.patch("api/task", (req, res) => {
+	app.patch("/api/task", (req, res) => {
 		db.Task.update(
 			{
 				title: req.body.title,
