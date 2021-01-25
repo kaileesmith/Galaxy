2c44fd468c5bfac76f9e56d626a728de5d62a2fa
Sam Ayler <sayler8701@gmail.com>
2021-01-25 10:40:50 -0800

    updating table for task removing time stamp

--- models/tasks.js
+++ models/tasks.js
@@ -1,29 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-	const Task = sequelize.define("Task", {
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				len: [1],
-			},
-		},
-		description: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		housemember: {
-			type: DataTypes.STRING,
-			allowNull: true,
-		},
-	}); 
-
-	Task.associate = function (models) {
-		Task.belongsTo(models.User, {
-			foreignKey: {
+	const Task = sequelize.define(
+		"Task",
+		{
+			title: {
+				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					len: [1],
+				},
 			},
-		});
-		
+			description: {
+				type: DataTypes.TEXT,
+				allowNull: true,
+			},
+			housemember: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
+
+	Task.associate = (models) => {
+		Task.belongsTo(models.User);
 	};
 	return Task;
 };
