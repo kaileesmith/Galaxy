fe6d5d1bb9e153295bfa33510f181940f38e060a
Kailee Smith <kaileesmith1501@gmail.com>
2021-01-25 21:00:55 -0800

    added delete for task

--- public/js/members.js
+++ public/js/members.js
@@ -72,6 +72,7 @@ $(document).ready(function () {
 	const choreInput = $("input#chore-title-input");
 	const choreDescription = $("textarea#chore-description-input");
 	const memberSelected = $("select#houseMember");
+	
 
 	// Event listener for button to add new task
 
@@ -104,3 +105,34 @@ $(document).ready(function () {
 			.catch(handleLoginErr);
 	}
 });
+
+const deleteBtn2 = document.querySelectorAll(".delete2");
+
+// Event listener for button to delete new user
+deleteBtn2.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		const id = e.currentTarget.getAttribute("data-id");
+		console.log(id);
+		fetch(`/api/task/${id}`, {
+			method: "DELETE",
+		});
+		location.reload("/");
+	});
+});
+
+// Function to delete a task
+function deleteTask(id) {
+	$.delete("/api/task/:id", {
+		id: id,
+	})
+		.then((data) => {
+			console.log("A task was deleted!");
+			location.reload("/");
+		})
+		.console.log("completed");
+}
+
+
+
